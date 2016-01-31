b0da39a929c3ca127c600f9322d639185c97c099
matthewpalmer <matt@matthewpalmer.net>
2016-01-31 20:32:17 +1100

    Change email boundary

--- MailingListParser/MailingListParser.swift
+++ MailingListParser/MailingListParser.swift
@@ -17,8 +17,48 @@ public class MailingListParser: NSObject {
     }
     
     public lazy var emails: [String] = {
-        return (self.string as NSString)
-            .componentsSeparatedByString("-------------- next part --------------")
-            .map { $0.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet()) }
+        // TODO: Some emails don't have the extra newline... might be a problem
+        let lines: [NSString] = (self.string as NSString).componentsSeparatedByString("\n")
+        
+        var beginningIndexOfEmails: [Int] = []
+        
+        var j: Int
+        
+        for i in 0..<lines.count {
+            let line = lines[i]
+            // We've definitely hit a new message. Now go back to the previous line that was empty.
+            if line.hasPrefix("Message-ID:") {
+                j = i
+                while j > 0 && lines[j].stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet()) != "" {
+                    j--
+                }
+                
+                // j is now at the beginning of an email
+                beginningIndexOfEmails.append(j)
+            }
+        }
+        
+        // beginningIndexOfEmails now partitions `lines` into emails.
+        var emails: [String] = []
+        
+        for i in 0..<beginningIndexOfEmails.count {
+            let beginningIndex = beginningIndexOfEmails[i]
+            
+            let endIndex: Int
+            if i == beginningIndexOfEmails.count - 1 {
+                endIndex = lines.count - 1
+            } else {
+                endIndex = beginningIndexOfEmails[i + 1]
+            }
+            
+            let slice = lines[beginningIndex..<endIndex].map { $0 as String }
+            let fullEmail = Array<String>(slice).joinWithSeparator("\n")
+            let trimmedEmail = (fullEmail as NSString)
+                .componentsSeparatedByString("-------------- next part --------------").first ?? ""
+            
+            emails.append(trimmedEmail.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))
+        }
+        
+        return emails
     }()
 }
